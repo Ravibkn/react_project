e40c172a4ec1e208d7870ce653c8d49938611593
unknown <ravijoshibkn@gmail.com>
2022-08-18 18:09:49 +0530

    chapter2

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import React from "react";
-import {View,Text,StyleSheet,Button}from 'react-native'
+import { View, Text, StyleSheet, Button, Linking } from 'react-native'
 
 
-const App =()=>{
+const App = () => {
   return (
     <View style={styles.body}>
       <Text style={styles.text}>Programing With Monu</Text>
+      <Button title='Login' onPress={() => { Linking.openURL('https://www.youtube.com/watch?v=ANdSdIlgsEw&list=PLJQbNrpYMOD5WE1kNfdgakkqzUNymtjlS&index=29&t=2027s') }}></Button>
     </View>
   )
 }
 
-const styles =StyleSheet.create({
-  body:{
-    flex:1,
-    backgroundColor:'green',
-    alignItems:'center',
-    justifyContent:'center'
-    
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center'
+
   },
-  text:{
-    fontSize:20,
-    color:'white',
+  text: {
+    fontSize: 20,
+    color: 'white',
+    margin:10,
   }
 })
 
