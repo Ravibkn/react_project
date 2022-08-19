118463a95c490d8e62b6ba406e35abc278c39bb4
unknown <ravijoshibkn@gmail.com>
2022-08-19 17:40:33 +0530

    chapter -6

--- App.js
+++ App.js
@@ -1,109 +1,58 @@
-import React from "react";
-import {View,Text,StyleSheet,Button}from 'react-native'
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button, ScrollView } from 'react-native'
 
 
-const App =()=>{
+const App = () => {
+
+  const [items, setItems] = useState([
+    { key: 1, item: 'item 1' },
+    { key: 2, item: 'item 2' },
+    { key: 3, item: 'item 3' },
+    { key: 4, item: 'item 4' },
+    { key: 5, item: 'item 5' },
+    { key: 6, item: 'item 6' },
+    { key: 7, item: 'item 7' },
+    { key: 8, item: 'item 8' },
+    { key: 9, item: 'item 9' },
+    { key: 10, item: 'item 10' },
+    { key: 11, item: 'item 11' },
+
+  ])
+
   return (
-    <View style={styles.body}>
-      <View style={styles.row}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-      <View style={styles.bigrow}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>   
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>   
-      </View>
-      
-      
-      
-    </View>
+
+    <ScrollView  style={styles.body}>
+      {
+        items.map((i) => {
+          return (
+            <View style={styles.item} key={i.key}>
+              <Text style={styles.text}>{i.item}</Text>
+            </View>
+          )
+        })
+      }
+    </ScrollView>
+
   )
 }
 
 const styles = StyleSheet.create({
-  body:{
-    flex:1,
-    backgroundColor:'blue',
-    alignItems:'center',
-    justifyContent:'flex-start',
+  body: {
+   
+    backgroundColor: '#ffff',
+
   },
-  row:{
-    backgroundColor:'#fff',
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'center',
-  }, bigrow:{
-    flex:1,
-    backgroundColor:'#fff',
-    flexDirection:'row',
-    alignItems:'stretch',
-    justifyContent:'center',
+  item: {
+    margin: 10,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
-  view1:{
-    flex:1,
-    backgroundColor:'green',
-    alignItems:'center',
-    justifyContent:'center',
-  },view2:{
-    flex:2,
-    backgroundColor:'red',
-    alignItems:'center',
-    justifyContent:'center',
-  },view3:{
-    flex:3,
-    backgroundColor:'yellow',
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  view4:{
-    flex:1,
-    backgroundColor:'brown',
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  view5:{
-    flex:1,
-    backgroundColor:'pink',
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  view6:{
-    flex:1,
-    backgroundColor:'white',
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  view7:{
-    flex:1,
-    backgroundColor:'blue',
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  text:{
-    fontSize:35,
-    color:'#000000',
-    margin:10,
+  text: {
+    fontSize: 35,
+    color: '#000000',
+    margin: 10,
   }
 })
 
