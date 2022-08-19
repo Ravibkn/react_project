5bf76033b4b179d5d9ff943f73d2dfa6f786f982
unknown <ravijoshibkn@gmail.com>
2022-08-19 16:44:22 +0530

    chapter-5

--- App.js
+++ App.js
@@ -1,50 +1,109 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button, Linking } from 'react-native'
+import React from "react";
+import {View,Text,StyleSheet,Button}from 'react-native'
 
 
-const App = () => {
-  const [name, setName] = useState('Ravi')
-
-  const onClickedHendelar = () => {
-    setName('Monu')
-
-  }
+const App =()=>{
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Programing With {name}</Text>
-      <View style={styles.button}>
-        <Button title='Updte State' onPress={onClickedHendelar}></Button>
+      <View style={styles.row}>
+        <View style={styles.view1}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={styles.view2}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={styles.view3}>
+          <Text style={styles.text}>3</Text>
+        </View>
       </View>
-
+      <View style={styles.row}>
+        <View style={styles.view4}>
+          <Text style={styles.text}>4</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.view5}>
+          <Text style={styles.text}>5</Text>
+        </View>
+      </View>
+      <View style={styles.bigrow}>
+        <View style={styles.view6}>
+          <Text style={styles.text}>6</Text>
+        </View>   
+        <View style={styles.view7}>
+          <Text style={styles.text}>7</Text>
+        </View>   
+      </View>
+      
+      
+      
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  body: {
-    // flex: 1,
-    width: '100%',
-    height: '50%',
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 10,
-    borderColor: 'red',
-    borderRadius: 40,
-
-    // margin:40,
-
-
+  body:{
+    flex:1,
+    backgroundColor:'blue',
+    alignItems:'center',
+    justifyContent:'flex-start',
   },
-  text: {
-    fontSize: 20,
-    color: 'white',
-    margin: 10,
+  row:{
+    backgroundColor:'#fff',
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+  }, bigrow:{
+    flex:1,
+    backgroundColor:'#fff',
+    flexDirection:'row',
+    alignItems:'stretch',
+    justifyContent:'center',
   },
-  button:{
-    width:200,
-    height:60,
-    borderRadius:10,
+  view1:{
+    flex:1,
+    backgroundColor:'green',
+    alignItems:'center',
+    justifyContent:'center',
+  },view2:{
+    flex:2,
+    backgroundColor:'red',
+    alignItems:'center',
+    justifyContent:'center',
+  },view3:{
+    flex:3,
+    backgroundColor:'yellow',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  view4:{
+    flex:1,
+    backgroundColor:'brown',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  view5:{
+    flex:1,
+    backgroundColor:'pink',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  view6:{
+    flex:1,
+    backgroundColor:'white',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  view7:{
+    flex:1,
+    backgroundColor:'blue',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  text:{
+    fontSize:35,
+    color:'#000000',
+    margin:10,
   }
 })
 
